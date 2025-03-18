7e1aa89ca5268f61fd9ff875061063051b483dca
alnah <alexis.nahan@gmail.com>
2025-03-18 11:56:45 -0300

    feat: add database config

---
+++ internal/config/config.go
@@ -0,0 +1,69 @@
+package config
+
+import (
+	"encoding/json"
+	"os"
+	"path/filepath"
+)
+
+// configFileName represents the JSON configuration file name
+const configFileName = ".gatorconfig.json"
+
+// DatabaseConfig holds configuration values for the database
+type DatabaseConfig struct {
+	URL             string `json:"db_url"`
+	CurrentUserName string `json:"current_user_name"`
+}
+
+// SetUser configures the current user name for the database
+func (db *DatabaseConfig) SetUser(currentUserName string) error {
+	db.CurrentUserName = currentUserName
+	return write(*db)
+}
+
+// Read decodes the JSON database configuration from the configuration file path
+func Read() (DatabaseConfig, error) {
+	filePath, err := getConfigFilePath()
+	if err != nil {
+		return DatabaseConfig{}, err
+	}
+	file, err := os.Open(filePath)
+	if err != nil {
+		return DatabaseConfig{}, err
+	}
+	defer func() { _ = file.Close() }()
+	dec := json.NewDecoder(file)
+	var db DatabaseConfig
+	if err = dec.Decode(&db); err != nil {
+		return DatabaseConfig{}, err
+	}
+	return db, nil
+}
+
+// write encodes the JSON database configuration to the configuration file path
+func write(db DatabaseConfig) error {
+	filePath, err := getConfigFilePath()
+	if err != nil {
+		return err
+	}
+	file, err := os.Create(filePath)
+	if err != nil {
+		return err
+	}
+	defer func() { _ = file.Close }()
+	enc := json.NewEncoder(file)
+	enc.SetIndent("", " ") // pretty format
+	if err = enc.Encode(db); err != nil {
+		return err
+	}
+	return nil
+}
+
+// getConfigFilePath constructs the complete configuration file path
+func getConfigFilePath() (string, error) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+	return filepath.Join(home, configFileName), nil
+}
