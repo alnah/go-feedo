47848925a235a0b1aa6937a36fa1c3c18da55e75
alnah <alexis.nahan@gmail.com>
2025-03-19 14:28:42 -0300

    feat: outsource logged in logic into middleware

---
+++ middleware.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/alnah/go-gator/internal/database"
+)
+
+// middlewareLoggedIn allows to change the function signature of our handlers that
+// require a logged in user to accept a user as an argument and DRY up the code
+func middlewareLoggedIn(handler func(s *state, cmd command, user database.User) error) commandHandler {
+	return func(s *state, cmd command) error {
+		user, err := s.dbQr.GetUser(context.Background(), s.dbCfg.CurrentUserName)
+		if err != nil {
+			return fmt.Errorf("couldn't get the current user: %w", err)
+		}
+		return handler(s, cmd, user)
+	}
+}
