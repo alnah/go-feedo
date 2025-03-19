e804627182ab1b7f967fa1b6fda686051b3d406f
alnah <alexis.nahan@gmail.com>
2025-03-19 11:49:36 -0300

    refactor: outsource reset handler in dedicated file
    refactor: outsource agg handler in dedicated file
    
    refactor: outsource handler agg to dedicated file

---
+++ handler_reset.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"context"
+	"fmt"
+)
+
+// handlerReset delete all the users from the users table
+// it's really to work on CRUD operations, of course never do that into a real app...
+func handlerReset(s *state, cmd command) error {
+	err := s.dbQr.DeleteUsers(context.Background())
+	if err != nil {
+		return fmt.Errorf("couldn't delete users: %w", err)
+	}
+	fmt.Println("Database reset successfully!")
+	return nil
+}
