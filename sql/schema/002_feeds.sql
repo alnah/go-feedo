b676f206f5b5dc2973e71895f6abaec837df6446
alnah <alexis.nahan@gmail.com>
2025-03-19 11:34:17 -0300

    refactor: use text instead of varchar, add not null

--- sql/schema/002_feeds.sql
+++ sql/schema/002_feeds.sql
@@ -1,11 +1,11 @@
 -- +goose Up
 CREATE TABLE feeds (
-  id UUID PRIMARY KEY,
-  created_at TIMESTAMP,
-  updated_at TIMESTAMP,
-  name VARCHAR(100) NOT NULL UNIQUE,
-  url VARCHAR(2048) NOT NULL UNIQUE,
-  user_id UUID NOT NULL REFERENCES users (id) ON DELETE CASCADE
+    id UUID PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    name TEXT NOT NULL,
+    url TEXT NOT NULL UNIQUE,
+    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE
 );
 
 -- +goose Down
