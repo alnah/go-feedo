f104db8aaa52500b34b977aecd66baf31bac026b
alnah <alexis.nahan@gmail.com>
2025-03-19 11:35:50 -0300

    style: improve clarity

--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -1,17 +1,21 @@
 -- name: CreateUser :one
 INSERT INTO users (id, created_at, updated_at, name)
-VALUES ($1, $2, $3, $4)
+VALUES (
+    $1,
+    $2,
+    $3,
+    $4
+)
 RETURNING *;
 
--- name: GetUserByName :one
-SELECT * FROM users WHERE name LIKE $1 LIMIT 1;
+-- name: GetUser :one
+SELECT * FROM users WHERE name = $1;
 
--- name: GetUserByID :one
-SELECT * FROM users WHERE id = $1;
-
--- name: DeleteAllUsers :exec
+-- name: DeleteUsers :exec
 DELETE FROM users;
 
--- name: GetAllUsers :many
-SELECT * FROM users ORDER BY id LIMIT $1 OFFSET $2;
+-- name: GetUsers :many
+SELECT * FROM users;
 
+-- name: GetUserById :one
+SELECT * FROM users WHERE id = $1;
