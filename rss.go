abf8ff4dad959474fef19338e0ee98d657bd78ba
alnah <alexis.nahan@gmail.com>
2025-03-19 07:55:06 -0300

    docs: add docstrings

--- rss.go
+++ rss.go
@@ -10,22 +10,34 @@ import (
 	"time"
 )
 
+// RSSFeed represents an RSS feed parsed from XML
 type RSSFeed struct {
+	// Channel contains metadata and items of the RSS feed
 	Channel struct {
-		Title       string    `xml:"title"`
-		Link        string    `xml:"link"`
-		Description string    `xml:"description"`
-		Item        []RSSItem `xml:"item"`
+		// Title is the feed's title
+		Title string `xml:"title"`
+		// Link is the URL to the feed
+		Link string `xml:"link"`
+		// Description provides details about the feed
+		Description string `xml:"description"`
+		// Item is the list of feed entries
+		Item []RSSItem `xml:"item"`
 	} `xml:"channel"`
 }
 
+// RSSItem represents an individual entry in an RSS feed
 type RSSItem struct {
-	Title       string `xml:"title"`
-	Link        string `xml:"link"`
+	// Title of the feed item
+	Title string `xml:"title"`
+	// Link to the full content of the item
+	Link string `xml:"link"`
+	// Description or summary of the item
 	Description string `xml:"description"`
-	PubDate     string `xml:"pubDate"`
+	// PubDate is the publication date of the item
+	PubDate string `xml:"pubDate"`
 }
 
+// fetchFeed retrieves and parses an RSS feed from the specified URL using the provided context.
 func fetchFeed(ctx context.Context, feedURL string) (*RSSFeed, error) {
 	req, err := http.NewRequestWithContext(ctx, "GET", feedURL, nil)
 	if err != nil {
