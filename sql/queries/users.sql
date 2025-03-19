2da75068b49f45b15454aa323d7fb0590981022a
alnah <alexis.nahan@gmail.com>
2025-03-19 07:48:22 -0300

    feat: get user by id

--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -6,6 +6,9 @@ RETURNING *;
 -- name: GetUser :one
 SELECT * FROM users WHERE name LIKE $1 LIMIT 1;
 
+-- name: GetUserByID :one
+SELECT * FROM users WHERE id = $1;
+
 -- name: DeleteAllUsers :exec
 DELETE FROM users;
 
